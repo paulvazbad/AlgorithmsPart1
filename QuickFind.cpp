268bc59752fcd8f7262d2b23bf91e5b1b478cf37
Paul Vazquez <pev@live.com.mx>
2020-09-15 19:59:25 -0700

    Added weighted implementation

--- QuickFind.cpp
+++ QuickFind.cpp
@@ -5,6 +5,7 @@ class QuickUnion
 private:
     int *_id;
     int _number_of_elements;
+    int *size_of_trees;
 
 public:
     QuickUnion(int number_of_elements);
@@ -16,15 +17,20 @@ public:
 QuickUnion::QuickUnion(int number_of_elements)
 {
     _id = new int[number_of_elements];
+    size_of_trees = new int[number_of_elements];
     for (int i = 0; i < number_of_elements; i++)
     {
         _id[i] = i;
+        size_of_trees[i] = 1;
     }
     _number_of_elements = number_of_elements;
 }
-int QuickUnion::root(int q){
-    while(_id[q]!= q){
-        q = _id[q];
+int QuickUnion::root(int q)
+{
+    while (_id[q] != q)
+    {
+        _id[q] = _id[_id[q]];
+            q = _id[q];
     }
     return q;
 }
@@ -38,13 +44,28 @@ void QuickUnion::create_union(int p, int q)
 {
     int root_p = root(p);
     int root_q = root(q);
-    _id[root_p] = root_q; 
+    if (root_p == root_q)
+        return;
+
+    if (size_of_trees[root_p] < size_of_trees[root_q])
+    {
+        _id[root_p] = root_q;
+        size_of_trees[root_q] += size_of_trees[root_p];
+    }
+    else
+    {
+        _id[root_q] = root_p;
+        size_of_trees[root_p] += size_of_trees[root_q];
+    }
+    _id[root_p] = root_q;
 }
 
-int main(){
+int main()
+{
     QuickUnion QF(5);
-    QF.create_union(2,3);
-    QF.create_union(3,1);
-    std::cout<<(QF.connected(2,1)? "YES" : "NO") <<endl;
+    QF.create_union(2, 3);
+    QF.create_union(3, 1);
+    std::cout << (QF.connected(2, 1) ? "YES" : "NO") << endl;
+
     return 0;
 }
