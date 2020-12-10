7c2bdbb6d09af57a336ea00b9c5d8776cb7e6279
Paul Vazquez <pev@live.com.mx>
2020-12-09 16:35:53 -0800

    Added unit tests

--- Week2/src/RandomizedQueue.h
+++ Week2/src/RandomizedQueue.h
@@ -1,16 +1,19 @@
 #ifndef RANDOMIZEDQUEUE_H
 #define RANDOMIZEDQUEUE_H
 
-#define FRIEND_TEST(test_case_name, test_name)\
-friend class test_case_name##_##test_name##_Test
+#include <stdlib.h> /* srand, rand */
+
+#define FRIEND_TEST(test_case_name, test_name) \
+    friend class test_case_name##_##test_name##_Test
 
 template <class T>
-class Element{
-    public:
+class Element
+{
+public:
     T data;
     bool valid;
+    Element() { valid = false; }
 };
-
 
 template <class T>
 class RandomizedQueue
@@ -20,14 +23,18 @@ public:
     bool isEmpty();
     int size();
     void enqueue(T);
+    T dequeue();
+    T sample();
 
 private:
     // Methods
     void resize(int);
+    int find_random();
 
     // For testing purposes
     FRIEND_TEST(RandomizedQueue_UT, resizingTest);
-
+    FRIEND_TEST(RandomizedQueue_UT, resizingShrinkTest);
+    FRIEND_TEST(RandomizedQueue_UT, fillingAndEmptying);
     // Members
     Element<T> *arr;
     int total_size;
@@ -62,11 +69,6 @@ void RandomizedQueue<T>::enqueue(T element)
         // Resize and grow
         resize(total_size * 2);
     }
-    if (current_size == total_size / 4 && current_size > 0)
-    {
-        // Resize and shrink
-        resize(total_size / 2);
-    }
     //Append element
     Element<T> e;
     e.data = element;
@@ -77,13 +79,60 @@ void RandomizedQueue<T>::enqueue(T element)
 template <class T>
 void RandomizedQueue<T>::resize(int new_size)
 {
+    if (new_size <= 0)
+        throw std::invalid_argument("Size of array 0");
+
     Element<T> *new_arr = new Element<T>[new_size];
     for (int i = 0; i < total_size; i++)
     {
-        new_arr[i] = arr[i];
+        if (arr[i].valid)
+            new_arr[i] = arr[i];
     }
     arr = new_arr;
     total_size = new_size;
+}
+
+template <class T>
+T RandomizedQueue<T>::sample()
+{
+    int index = find_random();
+    return arr[index].data;
+}
+
+template <class T>
+T RandomizedQueue<T>::dequeue()
+{
+    int index = find_random();
+    arr[index].valid = false;
+    T value_to_return = arr[index].data;
+    current_size--;
+    if (current_size == total_size / 4 && current_size > 0)
+    {
+        // Resize and shrink
+        resize(total_size / 2);
+    }
+    return value_to_return;
+}
+
+template <class T>
+int RandomizedQueue<T>::find_random()
+{
+    bool found = false;
+    int random_index;
+    int current_tries = 0;
+    if (size() == 0)
+        throw std::invalid_argument("Empty RandomizedQueue 0");
+    ;
+    while (current_tries < total_size)
+    {
+        random_index = rand() % total_size;
+        if (arr[random_index].valid == true)
+        {
+            return random_index;
+        }
+        current_tries++;
+    }
+    return -1;
 }
 
 #endif
--- Week2/test/RandomizedQueue-test.h
+++ Week2/test/RandomizedQueue-test.h
@@ -41,3 +41,55 @@ TEST_F(RandomizedQueue_UT, resizingTest)
     randoq->enqueue(50);
     ASSERT_EQ(randoq->total_size, 8);
 }
+
+TEST_F(RandomizedQueue_UT, resizingShrinkTest)
+{
+    randoq->enqueue(10);
+    ASSERT_EQ(randoq->total_size, 1);
+    randoq->enqueue(20);
+    ASSERT_EQ(randoq->total_size, 2);
+    randoq->enqueue(30);
+    ASSERT_EQ(randoq->total_size, 4);
+    randoq->enqueue(40);
+    ASSERT_EQ(randoq->total_size, 4);
+
+    cout << randoq->dequeue() << endl;
+    cout << randoq->dequeue() << endl;
+    cout << randoq->dequeue() << endl;
+    cout << randoq->dequeue() << endl;
+    randoq->enqueue(30);
+    cout << randoq->dequeue() << endl;
+}
+TEST_F(RandomizedQueue_UT, trydequeueOnEmpty)
+{
+    randoq->enqueue(10);
+
+    cout << randoq->dequeue() << endl;
+    EXPECT_THROW(randoq->dequeue(), std::invalid_argument);
+}
+
+TEST_F(RandomizedQueue_UT, fillingAndEmptying)
+{
+    for (int i = 0; i < MAX_TEST_SIZE; i++)
+    {
+        randoq->enqueue(i);
+        EXPECT_EQ(randoq->size(), i + 1);
+    }
+    for (int i = 0; i < MAX_TEST_SIZE - 1; i++)
+    {
+        randoq->dequeue();
+        EXPECT_EQ(randoq->size(), MAX_TEST_SIZE - i - 1);
+    }
+    EXPECT_EQ(randoq->total_size, 2);
+    randoq->dequeue();
+    EXPECT_EQ(randoq->total_size, 2);
+}
+TEST_F(RandomizedQueue_UT, sampleOneElement)
+{
+    
+    randoq->enqueue(10);
+    EXPECT_EQ(randoq->sample(), 10);
+    EXPECT_EQ(randoq->sample(), 10);
+    EXPECT_EQ(randoq->sample(), 10);
+    EXPECT_EQ(randoq->sample(), 10);
+}
